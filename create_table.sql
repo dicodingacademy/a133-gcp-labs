27de823d09282291d193f116016f9e276cf8acda
Fikri Helmi Setiawan <fikrihelmi17@gmail.com>
2021-12-17 06:41:41 +0000

    table scheme

--- create_table.sql
+++ create_table.sql
@@ -1,9 +1,3 @@
-mysql -h 34.101.180.100 -u root -p
-
-create database my_database;
-
-use my_database
-
 CREATE TABLE records (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(25) NOT NULL,
@@ -12,5 +6,3 @@ CREATE TABLE records (
     notes TEXT,
     attachment VARCHAR(255)
 );
-
-SHOW TABLES;

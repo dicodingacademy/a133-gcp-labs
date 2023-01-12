b1af03d92ab6cf22531ebb07b6941b0d4459eb05
Fikri Helmi Setiawan <fikrihelmi17@gmail.com>
2023-01-12 16:28:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello, world! Welcome to Google App Engine!')
+    res.send('Hello, world! Welcome to App Engine!')
 })
 
 const PORT = process.env.PORT || 8000
